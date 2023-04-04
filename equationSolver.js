c70f29b75bc84339984154b3e4a47f2b44ff5815
nikitq121 <nikitaq121@gmail.com>
2023-04-04 17:49:16 +0200

    Interactive mode
    An interactive mode was implemented.
     The program expects the input of coefficients from the user.
     A validator was implemented that does not allow incorrect data.

--- equationSolver.js
+++ equationSolver.js
@@ -13,3 +13,45 @@ const quadraticEquation = (a, b, c) => {
     ? `There is 1 root\n x1 = ${x1.toFixed(2)}`
     : `There are 2 roots\n x1 = ${x1.toFixed(2)}\n x2 = ${x2.toFixed(2)}`;
 };
+
+const validator = (a, b, c) => {
+  const numbers = [ a, b, c ];
+  return numbers.every((num => !isNaN(num))) && a !== 0;
+};
+
+const readline = require('readline');
+
+const getNumbersFromConsole = async () => {
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> ',
+  });
+  
+  rl.prompt();
+  
+  const question = (str) => new Promise((answer) => rl.question(str, answer));
+
+  const a = Number(await question('Enter a: '));
+  const b = Number(await question('Enter b: '));
+  const c = Number(await question('Enter c: '));
+
+  rl.close();
+  
+  return { a, b, c };
+};
+
+const FILENAME_POSITION = 3;
+
+(async () => {
+  const { a, b, c } = await getNumbersFromConsole();
+  const isValid = validator(a, b, c);
+
+  if (!isValid) console.log('One of number is not valid. Please enter valid numbers\nNote: a, b and c have to be numbers, a must not be zero');
+  else {
+    console.log(quadraticEquation(a, b, c));
+  }
+})();
+
+
